6e8771590d1db3a615820087bd802bd0d3525c3a
Ifwat Shamsuddin <ifwat.shamsuddin@gmail.com>
2023-03-26 18:05:19 +0800

    moved theme

---
+++ src/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from "@material-ui/core/styles"
+
+// Create a theme instance.
+const theme = createTheme({
+  palette: {
+    common: {
+      dark: "#dddce5",
+    },
+    primary: {
+      light: "#f1f3f5",
+      main: "#f5f7f8",
+      dark: "#f0f3f7",
+      contrastText: "#000",
+    },
+    secondary: {
+      light: "#d7dfe7",
+      main: "#d7dfe7",
+      dark: "#7e7e7e",
+      contrastText: "#fff",
+    },
+  },
+})
+
+export default theme
