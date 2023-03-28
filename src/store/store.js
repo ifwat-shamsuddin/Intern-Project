52b8b681fdf85b8c2056b474540b09e10591fb26
Ifwat Shamsuddin <ifwat.shamsuddin@gmail.com>
2023-03-28 10:38:09 +0800

    added dummy reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit"
 
-export default configureStore({
-	reducer: {},
-})
+// Created dummy reducer to remove the error below
+//
+// Store does not have a valid reducer.
+//
+// Make sure to remove this reducer when have a proper one
+const dummyReducer = (state = {}) => {
+  return state
+}
+
+export const makeStore = () => {
+  return configureStore({
+    reducer: { dummyReducer },
+  })
+}
+
+const store = makeStore()
+
+export default store
