0f010b8bfdb3548aca81e9326ea507e2a845ea9a
Ifwat Shamsuddin <ifwat.shamsuddin@gmail.com>
2023-04-11 16:06:37 +0800

    isolated number input component

---
+++ src/components/ControlledNumberInputField/ControlledNumberInputField.js
@@ -0,0 +1,53 @@
+import { makeStyles, TextField } from "@material-ui/core"
+import { Controller } from "react-hook-form"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    fontSize: theme.typography.fontSize,
+    fontFamily: theme.typography.fontFamily,
+
+    "& .MuiFormHelperText-contained": {
+      margin: "0",
+    },
+
+    "& .MuiOutlinedInput-input": {
+      padding: "6px 10px 6px 10px",
+      fontSize: theme.typography.fontSize,
+    },
+  },
+  label: {
+    fontWeight: theme.typography.fontWeightBold,
+    paddingBottom: "4px",
+  },
+}))
+
+const ControlledNumberInputField = ({ control, name, label, placeholder }) => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.root}>
+      <span className={classes.label}>{label}</span>
+
+      <Controller
+        control={control}
+        name={name}
+        render={({ value, onChange }) => {
+          return (
+            <TextField
+              value={value}
+              placeholder={placeholder}
+              variant="outlined"
+              onChange={(event) => onChange(event.target.value)}
+              size="small"
+              type="number"
+            />
+          )
+        }}
+      />
+    </div>
+  )
+}
+
+export default ControlledNumberInputField
