900460fdf54d32a235dd409e8b7557ff4254ef36
per-wat <ifwat.shamsuddin@gmail.com>
2023-05-05 14:14:46 +0800

    created selector for filtered character by id

--- src/selectors/characterSelectors.js
+++ src/selectors/characterSelectors.js
@@ -1,7 +1,20 @@
+import { wordsToRemoveEnum } from "@/enums/wordsToRemoveEnum"
+
 export const characters = (state) => state.character.characters
 
 export const getCharacterById = (state, id) => {
   const characters = state.character.characters
 
-  return characters.find((character) => character.id === id)
+  const character = characters.find((character) => character.id === id)
+  if (!character) return null
+
+  const filteredCharacter = {}
+  Object.keys(character).forEach((key) => {
+    const value = character[key]
+    filteredCharacter[key] = wordsToRemoveEnum.words.includes(value)
+      ? ""
+      : value
+  })
+
+  return filteredCharacter
 }
