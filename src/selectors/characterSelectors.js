fb6679ab757f581b0a414e473f2c1781816ac61c
per-wat <ifwat.shamsuddin@gmail.com>
2023-05-11 14:50:43 +0800

    removed filtering from selector

--- src/selectors/characterSelectors.js
+++ src/selectors/characterSelectors.js
@@ -1,20 +1,4 @@
-import { wordsToRemoveEnum } from "@/enums/wordsToRemoveEnum"
-
 export const characters = (state) => state.character.characters
 
-export const getCharacterById = (state, id) => {
-  const characters = state.character.characters
-
-  const character = characters.find((character) => character.id === id)
-  if (!character) return null
-
-  const filteredCharacter = {}
-  Object.keys(character).forEach((key) => {
-    const value = character[key]
-    filteredCharacter[key] = wordsToRemoveEnum.words.includes(value)
-      ? ""
-      : value
-  })
-
-  return filteredCharacter
-}
+export const getCharacterById = (state, id) =>
+  state.character.characters.find((character) => character.id === id)
