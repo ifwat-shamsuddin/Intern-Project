f5a5046bb478c24e5b9e8582b9cd2fcc28a2163f
per-wat <ifwat.shamsuddin@gmail.com>
2023-05-19 08:07:23 +0800

    added cell render function for respective columns

--- src/components/CharactersTable/CharactersTable.js
+++ src/components/CharactersTable/CharactersTable.js
@@ -1,22 +1,57 @@
 import { useSelector } from "react-redux"
 
 import CustomTable from "@/components/CustomTable"
+import StyledTableCell from "../CustomTable/StyledTableCell/StyledTableCell"
 import * as characterSelectors from "@/selectors/characterSelectors"
+import replaceIfNull from "@/utils/replaceIfNullUtils"
 
 const CharactersTable = ({ onRowClick }) => {
   const characters = useSelector(characterSelectors.characters)
 
   const columns = [
-    { header: "Name", field: "name" },
-    { header: "Eye Color", field: "eyeColor" },
-    { header: "Height", field: "height" },
-    { header: "Gender", field: "gender" },
-    { header: "Birth Year", field: "birthYear" },
-    { header: "Homeworld", field: "homeworld.name" },
-    { header: "Species", field: "species.name" },
+    {
+      header: "Name",
+      field: "name",
+      cellRenderer: ({ cellData }) => <StyledTableCell value={cellData} />,
+    },
+    {
+      header: "Eye Color",
+      field: "eyeColor",
+      cellRenderer: ({ cellData }) => <StyledTableCell value={cellData} />,
+    },
+    {
+      header: "Height",
+      field: "height",
+      cellRenderer: ({ cellData }) => <StyledTableCell value={cellData} />,
+    },
+    {
+      header: "Gender",
+      field: "gender",
+      cellRenderer: ({ cellData }) => (
+        <StyledTableCell value={replaceIfNull(cellData)} />
+      ),
+    },
+    {
+      header: "Birth Year",
+      field: "birthYear",
+      cellRenderer: ({ cellData }) => (
+        <StyledTableCell value={replaceIfNull(cellData)} />
+      ),
+    },
+    {
+      header: "Homeworld",
+      field: "homeworld.name",
+      cellRenderer: ({ cellData }) => <StyledTableCell value={cellData} />,
+    },
+    {
+      header: "Species",
+      field: "species.name",
+      cellRenderer: ({ cellData }) => <StyledTableCell value={cellData} />,
+    },
     {
       header: "Number of Films",
       field: "filmConnection.totalCount",
+      cellRenderer: ({ cellData }) => <StyledTableCell value={cellData} />,
     },
   ]
 
