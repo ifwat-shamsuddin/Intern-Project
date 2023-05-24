c0bb1ddaaa751bdd931bb40a77798758e39320bd
per-wat <ifwat.shamsuddin@gmail.com>
2023-05-24 12:53:55 +0800

    changed into template strings

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -1,7 +1,7 @@
 const actionPrefix = "characterActions/"
 
-export const ADD_CHARACTER = actionPrefix + "ADD_CHARACTER"
-export const EDIT_CHARACTER = actionPrefix + "EDIT_CHARACTER"
+export const ADD_CHARACTER = `${actionPrefix}ADD_CHARACTER`
+export const EDIT_CHARACTER = `${actionPrefix}EDIT_CHARACTER`
 
 export const addCharacter = (newCharacter) => {
   return {
