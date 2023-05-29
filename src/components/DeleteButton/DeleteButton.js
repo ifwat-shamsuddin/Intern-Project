2f8d72b763fe272a3da3a1d9b933e23e217dc25f
per-wat <ifwat.shamsuddin@gmail.com>
2023-05-30 01:05:02 +0800

    created button component

---
+++ src/components/DeleteButton/DeleteButton.js
@@ -0,0 +1,25 @@
+import { Button, makeStyles } from "@material-ui/core"
+import DeleteIcon from "@material-ui/icons/Delete"
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    backgroundColor: theme.palette.error.main,
+  },
+}))
+
+const DeleteButton = ({ label = "Delete", onClick }) => {
+  const classes = useStyles()
+
+  return (
+    <Button
+      variant="contained"
+      className={classes.button}
+      startIcon={<DeleteIcon />}
+      onClick={onClick}
+    >
+      {label}
+    </Button>
+  )
+}
+
+export default DeleteButton
