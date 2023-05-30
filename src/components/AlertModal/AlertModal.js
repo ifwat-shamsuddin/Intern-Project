c513e9e96a1dd2140b02779c33fc19935a221ab2
per-wat <ifwat.shamsuddin@gmail.com>
2023-05-30 11:18:19 +0800

    created general alert modal

---
+++ src/components/AlertModal/AlertModal.js
@@ -0,0 +1,36 @@
+import { Dialog, DialogTitle, makeStyles } from "@material-ui/core"
+import WarningRoundedIcon from "@material-ui/icons/WarningRounded"
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiDialog-paper": {
+      margin: 0,
+      padding: theme.spacing(2),
+      alignItems: "center",
+    },
+  },
+  title: {
+    color: theme.palette.error.main,
+  },
+}))
+
+const AlertModal = ({ isModalOpen, onClose, title = "Warning", children }) => {
+  const classes = useStyles()
+
+  return (
+    <Dialog
+      open={isModalOpen}
+      onClose={onClose}
+      className={classes.root}
+    >
+      <WarningRoundedIcon
+        color="error"
+        fontSize="large"
+      />
+      <DialogTitle className={classes.title}>{title}</DialogTitle>
+      {children}
+    </Dialog>
+  )
+}
+
+export default AlertModal
