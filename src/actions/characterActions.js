90063397caacf166c318db41dc2bb23395395189
per-wat <ifwat.shamsuddin@gmail.com>
2023-05-30 12:31:16 +0800

    created delete character action

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -2,6 +2,7 @@ const actionPrefix = "characterActions/"
 
 export const ADD_CHARACTER = `${actionPrefix}ADD_CHARACTER`
 export const EDIT_CHARACTER = `${actionPrefix}EDIT_CHARACTER`
+export const DELETE_CHARACTER = `${actionPrefix}DELETE_CHARACTER`
 
 export const addCharacter = (newCharacter) => {
   return {
@@ -14,5 +15,12 @@ export const editCharacter = (updatedCharacter) => {
   return {
     type: EDIT_CHARACTER,
     updatedCharacter,
+  }
+}
+
+export const deleteCharacter = (character) => {
+  return {
+    type: DELETE_CHARACTER,
+    character,
   }
 }
