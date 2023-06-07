078862114d8fda8a4b36dd93282dc51a4c3de9e2
per-wat <ifwat.shamsuddin@gmail.com>
2023-06-07 21:21:01 +0800

    adjusted query based on pagination

--- src/graphql/characterQueries.js
+++ src/graphql/characterQueries.js
@@ -1,26 +1,33 @@
 import { gql } from "@apollo/client"
 
 export const GET_ALL_CHARACTERS = gql`
-  query AllCharacters {
-    allPeople {
-      people {
-        id
-        name
-        eyeColor
-        height
-        gender
-        birthYear
-        homeworld {
+  query AllCharacters($first: Int, $cursor: String) {
+    allPeople(first: $first, after: $cursor) {
+      totalCount
+      edges {
+        node {
+          birthYear
+          eyeColor
+          filmConnection {
+            totalCount
+          }
+          gender
+          height
+          homeworld {
+            id
+            name
+          }
           id
           name
+          species {
+            id
+            name
+          }
         }
-        species {
-          id
-          name
-        }
-        filmConnection {
-          totalCount
-        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
       }
     }
   }
