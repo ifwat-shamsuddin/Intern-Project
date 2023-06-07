066bdd08810a738fa48a32b2e46ac179c1c0092a
per-wat <ifwat.shamsuddin@gmail.com>
2023-06-07 21:19:41 +0800

    added pagination row into table

--- src/components/CustomTable/CustomTable.js
+++ src/components/CustomTable/CustomTable.js
@@ -1,13 +1,21 @@
 import { Table, TableContainer, Paper } from "@material-ui/core"
+import { useState } from "react"
 
 import CustomizedTableHead from "./CustomizedTableHead"
 import StyledTableBody from "./StyledTableBody"
-import { useState } from "react"
 import CustomizedTablePagination from "./CustomizedTablePagination"
 
-const CustomTable = ({ columns, data, onRowClick, rowsPerPageOptions }) => {
+const CustomTable = ({
+  columns,
+  data,
+  onRowClick,
+  rowsPerPageOptions,
+  totalRowsCount,
+  rowsPerPage,
+  setRowsPerPage,
+  handleFetchMore,
+}) => {
   const [tablePage, setTablePage] = useState(0)
-  const [rowsPerPage, setRowsPerPage] = useState(rowsPerPageOptions?.[0] || 10)
 
   return (
     <TableContainer
@@ -27,11 +35,12 @@ const CustomTable = ({ columns, data, onRowClick, rowsPerPageOptions }) => {
 
         <CustomizedTablePagination
           rowsPerPageOptions={rowsPerPageOptions}
-          rowsCount={data.length}
+          totalRowsCount={totalRowsCount}
           rowsPerPage={rowsPerPage}
           tablePage={tablePage}
           setTablePage={setTablePage}
           setRowsPerPage={setRowsPerPage}
+          handleFetchMore={handleFetchMore}
         />
       </Table>
     </TableContainer>
